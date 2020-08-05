e5dee95ee571ef3d535b881425bf1390f1a97d5d
jssosa10 <js.sosa10@uniandes.edu.co>
2020-08-04 21:36:55 -0500

    new version of data model and resource

--- backends/gastos/app/models/gasto.py
+++ backends/gastos/app/models/gasto.py
@@ -5,6 +5,9 @@ class GastoModel(db.Model):
 
     __tablename__ = 'gastos'
 
+    """
+        datos basicos del gasto
+    """
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
     fecha_subido = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
@@ -12,26 +15,85 @@ class GastoModel(db.Model):
     tipo = db.Column(db.Integer, nullable=False)
     valor = db.Column(db.Integer, nullable=False)
     descripcion = db.Column(db.String(255), nullable=False)
+
+    """
+        datos del beneficiario
+    """
+    beneficiario_documento = db.Column(db.String(80), nullable=False)
+    beneficiario_nombre = db.Column(db.String(255), nullable=False)
+    beneficiario_telefono = db.Column(db.String(80), nullable=False)
+    beneficiario_ciudad = db.Column(db.String(80), nullable=False)
+    beneficiario_direccion  = db.Column(db.String(100), nullable=True)
+    beneficiario_placa = db.Column(db.String(10),nullable=True)
+
+
+    """
+        documentos de soporte
+    """
     identificacion = db.Column(db.String(120), nullable=False)
     documento = db.Column(db.String(120), nullable=False)
+    foto_planilla = db.Column(db.String(120), nullable=True)
+
+    """
+        datos de transporte
+    """
     origen = db.Column(db.String(255), nullable=True)
     destino = db.Column(db.String(255), nullable=True)
+    origen_cuidad = db.Column(db.String(255), nullable=True)
+    destino_ciudad = db.Column(db.String(255), nullable=True)
+    origen_departamento = db.Column(db.String(255), nullable=True)
+    destino_departamento = db.Column(db.String(255), nullable=True)
     medio = db.Column(db.String(255), nullable=True)
+
+    """
+        datos aprobacion
+    """
     aprobado = db.Column(db.Boolean, default=False, nullable=False)
 
-    def __init__(self, cedula, fecha, tipo, valor, descripcion, identificacion, documento, origen=None, destino=None, medio=None):
-        """Initialize the bucketlist with a name and its creator."""
+    def __init__(self, cedula, fecha, tipo, valor, descripcion,
+        identificacion, documento, origen, destino, medio, beneficiario_ciudad,
+        beneficiario_documento, beneficiario_direccion, beneficiario_nombre,
+        beneficiario_placa, beneficiario_telefono, foto_planilla, origen_cuidad,
+        origen_departamento,destino_ciudad, destino_departamento):
+
+        """Inicializar un gasto"""
+
+        """
+            datos basicos del gasto
+        """
         self.cedula = cedula
         self.fecha = fecha
         self.tipo = tipo
         self.valor = valor
         self.descripcion = descripcion
+
+        """
+            datos del beneficiario
+        """
+        self.beneficiario_documento = beneficiario_documento
+        self.beneficiario_nombre = beneficiario_nombre
+        self.beneficiario_telefono = beneficiario_telefono
+        self.beneficiario_ciudad = beneficiario_ciudad
+        self.beneficiario_direccion  = beneficiario_direccion
+        self.beneficiario_placa = beneficiario_placa
+
+        """
+            documentos de soporte
+        """
         self.identificacion = identificacion
         self.documento = documento
-        if self.tipo==2:
-            self.origen = origen
-            self.destino = destino
-            self.medio = medio
+        self.foto_planilla = foto_planilla
+
+        """
+            datos transporte
+        """
+        self.origen = origen
+        self.destino = destino
+        self.medio = medio
+        self.origen_cuidad = origen_cuidad
+        self.destino_ciudad = destino_ciudad
+        self.origen_departamento = origen_departamento
+        self.destino_departamento = destino_departamento
 
     def json(self):
         return {
